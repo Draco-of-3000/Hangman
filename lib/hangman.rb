27f1a1a77fdc81aadfc6f26033da55f77049bab2
Draco <kevin.tenisson@gmail.com>
2023-06-19 04:21:01 -0700

    Created a class, transitioning to OOP

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,16 +1,29 @@
 puts "Hangman Hangman Hangmaaaanneee"
 
-def load_and_select_word_randomly
-    random_word = []
-    contents = File.open('google-10000-english-no-swears.txt', 'r') do |file|
-        file.each_line do |line|
-            words = line.split(' ')
-            words.select! { |word| word.length >= 5 && word.length <= 12}
-            random_word = words.sample
-            break if random_word
-        end
+
+
+class Hangman
+    attr_accessor :random_word, :number_of_guesses
+
+    def initialize
+        @random_word = random_word
+        @number_of_guesses = number_of_guesses
     end
 
-    puts random_word
+    def load_and_select_word_randomly
+        contents = File.open('google-10000-english-no-swears.txt', 'r') do |file|
+            file.each_line do |line|
+                words = line.split(' ')
+                words.select! { |word| word.length >= 5 && word.length <= 12}
+                @random_word = words.sample
+                break if @random_word
+            end
+        end
+    
+        puts @random_word
+    end
 end
+
+hangman = Hangman.new
+hangman.load_and_select_word_randomly
 
